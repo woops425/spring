6ca404163693105ef96cc1c92db6ceef84a9c44e
woops425 <162817809+woops425@users.noreply.github.com>
2025-01-31 20:32:32 +0900

    Update TravelClubApp.java

--- travel-club-spring-core/src/main/java/io/namoosori/travelclub/spring/TravelClubApp.java
+++ travel-club-spring-core/src/main/java/io/namoosori/travelclub/spring/TravelClubApp.java
@@ -1,7 +1,10 @@
 package io.namoosori.travelclub.spring;
 
+import io.namoosori.travelclub.spring.aggregate.club.CommunityMember;
 import io.namoosori.travelclub.spring.aggregate.club.TravelClub;
 import io.namoosori.travelclub.spring.service.ClubService;
+import io.namoosori.travelclub.spring.service.MemberService;
+import io.namoosori.travelclub.spring.service.sdo.MemberCdo;
 import io.namoosori.travelclub.spring.service.sdo.TravelClubCdo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -15,7 +18,20 @@ public class TravelClubApp {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         String [] beanNames = context.getBeanDefinitionNames();
-        System.out.println(Arrays.toString(beanNames));
+//        System.out.println(Arrays.toString(beanNames));
+
+        MemberService memberService = context.getBean("memberServiceLogic", MemberService.class);
+
+        String memberId = memberService.registerMember(
+                new MemberCdo(
+                        "test@nextree.io",
+                        "kim",
+                        "Test Member",
+                        "010-0000-0000",
+                        "2020.10.10"));
+
+        CommunityMember foundedMember = memberService.findMemberById(memberId);
+        System.out.println(foundedMember.toString());
 
 //        TravelClubCdo clubCdo = new TravelClubCdo("TravelClub", "Test TravelClub");
 //        ClubService clubService = context.getBean("clubService", ClubService.class);
